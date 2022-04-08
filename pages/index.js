9ca9b2a5b641c95f6c71629d478a49b72e482b3e
Matthew Morrison <james.m.morrison@vanderbilt.edu>
2022-04-08 12:19:06 -0500

    create todo list

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+const items = ["do homework", "clean room", "buy food"];
 
 export default function Home() {
+  const [list, setList] = useState(items);
+  const [item, setItem] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setList((prevList) => [...prevList, item]);
+    setItem("");
+  };
+
+  const handleChange = (e) => {
+    setItem(e.target.value);
+  };
+
+  const removeItem = (idx) => {
+    setList((prevList) => [
+      ...prevList.slice(0, idx),
+      ...prevList.slice(idx + 1),
+    ]);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +34,28 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>To Do List</h1>
+        <ul className={styles.list}>
+          {list.map((el, idx) => (
+            <li
+              className={styles.item}
+              key={idx}
+              onClick={() => removeItem(idx)}
+            >
+              {el}
+            </li>
+          ))}
+        </ul>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <input type="text" value={item} onChange={handleChange}></input>
+          <input
+            type="submit"
+            className={styles.button}
+            value="+ add item"
+          ></input>
+        </form>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
